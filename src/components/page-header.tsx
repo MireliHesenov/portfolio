4e2140d190ca3c177e3292e028e3ae9541e8008e
Firuzcan Hacıyev <firuz.haciyeff@gmail.com>
2025-08-19 00:21:14 +0400

    Add PageHeader component with title and description subcomponents for improved page structure and readability.

---
+++ src/components/page-header.tsx
@@ -0,0 +1,40 @@
+import { cn } from "@/lib/utils";
+
+function PageHeader({
+  children,
+  className,
+  ...props
+}: React.ComponentProps<"section">) {
+  return (
+    <section className={cn("flex flex-col gap-2 mb-12", className)} {...props}>
+      {children}
+    </section>
+  );
+}
+
+
+function PageHeaderTitle({
+  children,
+  className,
+  ...props
+}: React.ComponentProps<"h1">) {
+  return (
+    <h1 className={cn("text-3xl md:text-5xl font-bold", className)} {...props}>
+      {children}
+    </h1>
+  );
+}
+
+function PageHeaderDescription({
+  children,
+  className,
+  ...props
+}: React.ComponentProps<"p">) {
+  return (
+    <p className={cn("text-sm text-muted-foreground", className)} {...props}>
+      {children}
+    </p>
+  );
+}
+
+export { PageHeader, PageHeaderTitle, PageHeaderDescription };
