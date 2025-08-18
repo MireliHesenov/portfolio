419dfd6d1ae1a05522d088055c91b1f46e933e76
Firuzcan Hacıyev <firuz.haciyeff@gmail.com>
2025-08-18 22:20:43 +0400

    Refactor ThemeToggle component to use motion for animated theme transitions and improve structure by exposing the theme state.

--- src/widgets/theme-toggle.tsx
+++ src/widgets/theme-toggle.tsx
@@ -2,20 +2,33 @@
 
 import { useTheme } from "next-themes";
 
+import { motion } from "motion/react";
+
 import { MoonIcon, SunIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
-export default function ThemeToggle() {
-  const { setTheme } = useTheme();
+export function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
 
   const handleThemeToggle = () => {
     setTheme((prev) => (prev === "dark" ? "light" : "dark"));
   };
 
   return (
-    <Button variant="outline" size="icon" onClick={handleThemeToggle}>
-      <SunIcon className="block dark:hidden" />
-      <MoonIcon className="hidden dark:block" />
+    <Button
+      variant="outline"
+      size="icon"
+      className="relative overflow-clip"
+      onClick={handleThemeToggle}
+    >
+      <motion.span
+        initial={{ rotate: 0 }}
+        animate={{ rotate: theme !== "dark" ? 180 : 0 }}
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 w-8 h-8 origin-center"
+      >
+        <MoonIcon className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2" />
+        <SunIcon className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2" />
+      </motion.span>
     </Button>
   );
 }
